b06ff7168a9e2410d8795322685ded134602c0a2
Jaswanth <jassijaswanth360@gmail.com>
2026-06-13 15:23:00 +0530

    Implement Dockerfile for multi-stage build; integrate Clerk authentication in frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,33 @@ const express=require('express');
 const env=require('dotenv');
 const mongoose=require('mongoose');
 const cors=require('cors');
-
+const fs = require('fs');
+const path = require('path');
 import { clerkMiddleware } from '@clerk/express'
 const connectDB=require('./libs/db');
 
+const app=express();
 
 env.config();
 const PORT=process.env.PORT;
 const FRONTEND_URL=process.env.FRONTEND_URL;
 
-const app=express();
+const publicDir=path.join(process.cwd(),'public');
+
+
+
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
+
+
+if (!fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+    app.get("/{*any}",(req,res,next)=>{
+        res.sendFile(path.join(publicDir,"index.html"),(err)=>next(err));
+    });
+}
 
 app.listen(PORT,()=>{
     connectDB();
