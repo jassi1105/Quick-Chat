8526fe222195e8b27f4ee94f0b1401036a8a906b
Jaswanth <jassijaswanth360@gmail.com>
2026-06-13 16:22:35 +0530

    Refactor db.js to improve code formatting and ensure consistent use of ES module syntax

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -2,16 +2,19 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const MONGO_URI=process.env.MONGO_URI
+        const MONGO_URI = process.env.MONGO_URI;
+
         if (!MONGO_URI) {
             throw new Error('MONGO_URI is not defined in the environment variables');
         }
-        const conn=await mongoose.connect(MONGO_URI);
-        console.log('MongoDB connected',conn.connection.host);
+
+        const conn = await mongoose.connect(MONGO_URI);
+
+        console.log('MongoDB connected', conn.connection.host);
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
         process.exit(1);
     }
 };
 
-module.exports = connectDB;
+export default connectDB;
